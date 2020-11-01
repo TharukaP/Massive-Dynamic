872a9d545e1aab32d8afbe2017efd2b357b793c9
TharukaP <it20774134@gmail.com>
2020-11-01 13:27:32 +0530

    Mechanic Login UI updated.

--- src/Frontend/Mechanic_Login.java
+++ src/Frontend/Mechanic_Login.java
@@ -30,26 +30,21 @@ public class Mechanic_Login extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
         txtUsername = new javax.swing.JTextField();
-        txtPassword = new javax.swing.JPasswordField();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        txtPassword = new javax.swing.JPasswordField();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Frontend/resources/username.png"))); // NOI18N
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 160, -1, -1));
-
-        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Frontend/resources/password.png"))); // NOI18N
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 280, -1, -1));
-        getContentPane().add(txtUsername, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 160, 296, 48));
-        getContentPane().add(txtPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 290, 300, 50));
+        getContentPane().setLayout(null);
+        getContentPane().add(txtUsername);
+        txtUsername.setBounds(400, 120, 220, 40);
 
         jButton1.setFont(new java.awt.Font("Sylfaen", 1, 24)); // NOI18N
         jButton1.setText("Login");
@@ -58,7 +53,8 @@ public class Mechanic_Login extends javax.swing.JFrame {
                 jButton1ActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(660, 430, 130, 60));
+        getContentPane().add(jButton1);
+        jButton1.setBounds(320, 400, 110, 40);
 
         jButton2.setFont(new java.awt.Font("Sylfaen", 1, 24)); // NOI18N
         jButton2.setText("Home");
@@ -67,19 +63,36 @@ public class Mechanic_Login extends javax.swing.JFrame {
                 jButton2ActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 430, 130, 60));
-
-        jLabel4.setIcon(new javax.swing.ImageIcon("C:\\Users\\Udaya\\OneDrive\\Desktop\\Pictures\\user.png")); // NOI18N
-        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 150, -1, -1));
+        getContentPane().add(jButton2);
+        jButton2.setBounds(500, 400, 110, 40);
+        getContentPane().add(jLabel4);
+        jLabel4.setBounds(70, 150, 0, 0);
 
         jLabel5.setFont(new java.awt.Font("Sylfaen", 1, 36)); // NOI18N
         jLabel5.setText("Mechanic login");
-        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 50, -1, -1));
+        getContentPane().add(jLabel5);
+        jLabel5.setBounds(330, 40, 261, 48);
 
-        jLabel6.setIcon(new javax.swing.ImageIcon("C:\\Users\\Udaya\\OneDrive\\Desktop\\Pictures\\background3.png")); // NOI18N
-        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(-50, 0, 940, 560));
+        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Frontend/resources/username.png"))); // NOI18N
+        getContentPane().add(jLabel6);
+        jLabel6.setBounds(320, 120, 64, 64);
 
-        setBounds(0, 0, 942, 625);
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Frontend/resources/password.png"))); // NOI18N
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(320, 230, 64, 64);
+        getContentPane().add(txtPassword);
+        txtPassword.setBounds(400, 250, 220, 40);
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Frontend/resources/user.png"))); // NOI18N
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(20, 80, 260, 270);
+
+        jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Frontend/resources/mechanicloigbackgrou2.jpg"))); // NOI18N
+        jLabel7.setText("jLabel7");
+        getContentPane().add(jLabel7);
+        jLabel7.setBounds(0, -40, 660, 570);
+
+        setBounds(0, 0, 663, 497);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
@@ -150,11 +163,12 @@ public class Mechanic_Login extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
     private javax.swing.JPasswordField txtPassword;
     private javax.swing.JTextField txtUsername;
     // End of variables declaration//GEN-END:variables
