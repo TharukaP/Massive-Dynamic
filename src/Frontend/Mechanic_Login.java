c08e6755bb2b6b44542aa7bb989b305e41a6924c
Nimesha Perera <>
2020-11-24 17:10:16 +0530

    Mechanic UI updated

--- src/Frontend/Mechanic_Login.java
+++ src/Frontend/Mechanic_Login.java
@@ -4,13 +4,11 @@
  * and open the template in the editor.
  */
 package Frontend;
-
 import java.awt.event.WindowEvent;
 import javax.swing.JOptionPane;
-
 /**
  *
- * @author Tharuka Perera
+ * @author Nimesha Perera
  */
 public class Mechanic_Login extends javax.swing.JFrame {
 
@@ -19,6 +17,8 @@ public class Mechanic_Login extends javax.swing.JFrame {
      */
     public Mechanic_Login() {
         initComponents();
+        setSize(1020,700);
+        setResizable(false);
     }
 
     /**
@@ -30,21 +30,45 @@ public class Mechanic_Login extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
         txtUsername = new javax.swing.JTextField();
+        txtPassword = new javax.swing.JPasswordField();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
-        jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
-        txtPassword = new javax.swing.JPasswordField();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        getContentPane().setLayout(null);
-        getContentPane().add(txtUsername);
-        txtUsername.setBounds(400, 120, 220, 40);
+
+        jPanel1.setLayout(null);
+
+        jLabel1.setFont(new java.awt.Font("Sylfaen", 1, 48)); // NOI18N
+        jLabel1.setText("Mechanic Login");
+        jPanel1.add(jLabel1);
+        jLabel1.setBounds(510, 90, 430, 64);
+
+        jLabel2.setIcon(new javax.swing.ImageIcon("C:\\Users\\Udaya\\Downloads\\Telegram Desktop\\Automart3\\Automart3\\src\\Frontend\\resources\\user.png")); // NOI18N
+        jPanel1.add(jLabel2);
+        jLabel2.setBounds(60, 220, 270, 270);
+
+        jLabel3.setIcon(new javax.swing.ImageIcon("C:\\Users\\Udaya\\Downloads\\Telegram Desktop\\Automart3\\Automart3\\src\\Frontend\\resources\\username.png")); // NOI18N
+        jPanel1.add(jLabel3);
+        jLabel3.setBounds(410, 220, 80, 80);
+
+        jLabel4.setIcon(new javax.swing.ImageIcon("C:\\Users\\Udaya\\Downloads\\Telegram Desktop\\Automart3\\Automart3\\src\\Frontend\\resources\\password.png")); // NOI18N
+        jPanel1.add(jLabel4);
+        jLabel4.setBounds(410, 390, 70, 90);
+
+        txtUsername.setFont(new java.awt.Font("Sylfaen", 0, 24)); // NOI18N
+        jPanel1.add(txtUsername);
+        txtUsername.setBounds(580, 230, 360, 60);
+
+        txtPassword.setFont(new java.awt.Font("Sylfaen", 0, 24)); // NOI18N
+        jPanel1.add(txtPassword);
+        txtPassword.setBounds(580, 410, 360, 60);
 
         jButton1.setFont(new java.awt.Font("Sylfaen", 1, 24)); // NOI18N
         jButton1.setText("Login");
@@ -53,8 +77,8 @@ public class Mechanic_Login extends javax.swing.JFrame {
                 jButton1ActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton1);
-        jButton1.setBounds(320, 400, 110, 40);
+        jPanel1.add(jButton1);
+        jButton1.setBounds(420, 560, 120, 60);
 
         jButton2.setFont(new java.awt.Font("Sylfaen", 1, 24)); // NOI18N
         jButton2.setText("Home");
@@ -63,40 +87,30 @@ public class Mechanic_Login extends javax.swing.JFrame {
                 jButton2ActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton2);
-        jButton2.setBounds(500, 400, 110, 40);
-        getContentPane().add(jLabel4);
-        jLabel4.setBounds(70, 150, 0, 0);
+        jPanel1.add(jButton2);
+        jButton2.setBounds(810, 560, 130, 60);
 
-        jLabel5.setFont(new java.awt.Font("Sylfaen", 1, 36)); // NOI18N
-        jLabel5.setText("Mechanic login");
-        getContentPane().add(jLabel5);
-        jLabel5.setBounds(330, 40, 261, 48);
+        jLabel5.setIcon(new javax.swing.ImageIcon("C:\\Users\\Udaya\\OneDrive\\Desktop\\Pictures\\background3.png")); // NOI18N
+        jPanel1.add(jLabel5);
+        jLabel5.setBounds(0, 0, 1160, 740);
 
-        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Frontend/resources/username.png"))); // NOI18N
-        getContentPane().add(jLabel6);
-        jLabel6.setBounds(320, 120, 64, 64);
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 1086, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 740, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Frontend/resources/password.png"))); // NOI18N
-        getContentPane().add(jLabel1);
-        jLabel1.setBounds(320, 230, 64, 64);
-        getContentPane().add(txtPassword);
-        txtPassword.setBounds(400, 250, 220, 40);
-
-        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Frontend/resources/user.png"))); // NOI18N
-        getContentPane().add(jLabel2);
-        jLabel2.setBounds(20, 80, 260, 270);
-
-        jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Frontend/resources/mechanicloigbackgrou2.jpg"))); // NOI18N
-        jLabel7.setText("jLabel7");
-        getContentPane().add(jLabel7);
-        jLabel7.setBounds(0, -40, 650, 510);
-
-        setBounds(0, 0, 663, 496);
+        pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-              String username=txtUsername.getText();
+        // TODO add your handling code here:
+        String username=txtUsername.getText();
         String password=txtPassword.getText();
         
         if(username.contains("admin")&&(password.contains("12345"))){
@@ -104,7 +118,9 @@ public class Mechanic_Login extends javax.swing.JFrame {
             txtPassword.setText(null);
             systemExit();
             
-           
+           Mechanic_UI page=new Mechanic_UI();
+            page.setVisible(true);
+            this.setVisible(false);
             
         }
         else{
@@ -113,10 +129,10 @@ public class Mechanic_Login extends javax.swing.JFrame {
             txtPassword.setText(null);
             
         }
-        
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
         systemExit();
         StaffPortal page1=new StaffPortal();
         page1.setVisible(true);
@@ -149,6 +165,7 @@ public class Mechanic_Login extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(Mechanic_Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -163,18 +180,15 @@ public class Mechanic_Login extends javax.swing.JFrame {
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JPasswordField txtPassword;
     private javax.swing.JTextField txtUsername;
     // End of variables declaration//GEN-END:variables
 
-   
-
-private void systemExit(){
-    WindowEvent winClosing= new WindowEvent(this,WindowEvent.WINDOW_CLOSED); 
-}
-
+    private void systemExit() {
+        WindowEvent winClosing= new WindowEvent(this,WindowEvent.WINDOW_CLOSED); 
+    }
 }
